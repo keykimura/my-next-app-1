c614611298d4150a81c006719506ac5fe3dcb0c0
kengo <kenrockman66@gmail.com>
2024-11-28 11:18:12 +0900

    変更を加えました

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 // app/page.tsx
 
-import Header from "../components/Header";
-import Profile from "../components/Profile";
-import Buttons from "../components/Buttons";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+import Buttons from "./components/Buttons";
 import styles from "./page.module.css";
-import Sidebar from "@/components/Sidebar";
-import Footer from "@/components/Footer";
+import Sidebar from "@/app/components/Sidebar";
+import Footer from "@/app/components/Footer";
 
 export default function Home() {
   return (
